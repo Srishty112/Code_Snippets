9247bd160e6322b4b82b6cfaa2f1e7ab45a7c976
Srishty112 <78474742+Srishty112@users.noreply.github.com>
2021-10-19 18:07:09 +0530

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,6 +1,3 @@
-#include "bits/stdc++.h"
-using namespace std;
-
 void quickSort(int arr[], int l, int r)
 {
     if(l<r)
@@ -17,15 +14,4 @@ void quickSort(int arr[], int l, int r)
         quickSort(arr, l, i); 
         quickSort(arr, i+2, r); 
     }
-}
-
-int main()
-{
-    int n; cin>>n;
-    int arr[n];
-    for(int i=0; i<n; i++)
-    cin>>arr[i];
-    quickSort(arr,0, n-1);
-    for(int i=0; i<n; i++)
-    cout<<arr[i]<<"  ";
 }
